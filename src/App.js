88aedb25b8fb709a0e6da685e9c093d7bb466f7f
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-26 12:57:12 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import './Constants.scss';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+            <div className="App">
+          <h1 className="logo">MyFace</h1>
+              
+              <Switch>
+                  <Route path="/posts">
+                          {/*<PostsPage/>*/}
+                  <div className="all-posts-container">
+                      <header className="App-header">
+                          All Posts
+                      </header>
+                  </div>
+                  </Route>
+                  
+                  <Route path="/users">
+                      <div className="all-users-container">
+                          <header className="App-header">
+                              All Users
+                          </header>
+                      </div> 
+                  </Route>
+              
+      </Switch>
+          </div>
+      </Router>
   );
 }
-
-export default App;
+    export default App;
---
+++ src/Navigation/Nav.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import './App.scss';
+import './Constants.scss';
+
+function Nav() {
+    return (
+        <link/>
+    )
+}
