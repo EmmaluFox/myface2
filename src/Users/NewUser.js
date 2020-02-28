8cfe0ef137164d0eb3f2531509866429591b5e17
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-28 16:15:34 +0000

    Latest

--- src/Users/NewUser.js
+++ src/Users/NewUser.js
@@ -4,6 +4,8 @@ export function NewUser() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email   , setEmail] = useState("");
+    const [profileImageUrl   , setProfileImageUrl] = useState("");
+    const [coverImageUrl   , setCoverImageUrl] = useState("");
     
     function newFirstName(event) {
         setFirstName(event.target.value);}
@@ -14,6 +16,12 @@ export function NewUser() {
     function newEmail(event) {
         setEmail(event.target.value);}
 
+    function newProfileImageUrl(event) {
+        setProfileImageUrl(event.target.value);}
+
+    function newCoverImageUrl(event) {
+        setCoverImageUrl(event.target.value);}
+
 
     function handleSubmit(event) {
     event.preventDefault(); event.send("https://localhost:5001/users")}
@@ -21,9 +29,11 @@ export function NewUser() {
     
             return (
                 <form className="newUserForm" onSubmit={handleSubmit}>
-                    <label>First Name:<input  type="text" value={firstName} onChange={newFirstName}/></label>
-                    <label>Last Name: <input  type="text" value={lastName} onChange={newLastName}/></label>
-                    <label>Email:  <input  type="text" value={email} onChange={newEmail}/></label>
-                        <input type="submit" value="Submit"/></form>)
+                    <label>First Name:  <input  type="text" value={firstName} onChange={newFirstName}/></label>
+                    <label>Last Name:   <input  type="text" value={lastName} onChange={newLastName}/></label>
+                    <label>Email:   <input  type="text" value={email} onChange={newEmail}/></label>
+                    <label>Profile Image Url:   <input  type="text" value={profileImageUrl} onChange={newProfileImageUrl}/></label>
+                    <label>Cover Image Url:   <input  type="text" value={coverImageUrl} onChange={newCoverImageUrl}/></label>
+                    <input type="submit" value="Submit"/></form>)
         ;}
                 
